5507848a98d171248d114f944ecc5fb882f56767
viper3400 <viper_3400@gmx.de>
2023-08-12 10:21:56 +0200

    (purge) Removed unused code

--- src/stories/homeweb/LoginForm.tsx
+++ src/stories/homeweb/LoginForm.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { getToken } from '@/lib/getToken'
 import { LoginCredentials } from '@/lib/types'
 import { TextInput } from './elements/TextInput'
 import { JaxxButton } from './elements/JaxxButton'
@@ -42,8 +41,6 @@ export const LoginForm = ({
     group: 'VG_Default'
   })
 
-  const [ token, setToken ] = useState('not set')
-
   const handleInputChange = (inputName: string, value: string) => {
     setFormData((prevData) => ({
       ...prevData,
@@ -54,9 +51,6 @@ export const LoginForm = ({
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     // Perform login logic using formData
-    /*const token = await getToken(
-      { username: formData.username, password: formData.password, group: 'VG_Default' })
-    setToken(token.token)*/
     // Raise the onSubmit event with the form data
     onSubmit(formData)
   }
@@ -88,8 +82,6 @@ export const LoginForm = ({
               type="submit"
               data-test="sign-in-button" />
           </div>
-          <div>{token}</div>
-          <div>x: {formData.password}</div>
         </form>
       </div>
 
