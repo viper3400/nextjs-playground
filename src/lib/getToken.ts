5838ef1c4d88b11c0749375dcbee8299909dcfe3
viper3400 <viper_3400@gmx.de>
2023-09-01 14:57:58 +0200

    first attempt to integrate swagger API

--- src/lib/getToken.ts
+++ src/lib/getToken.ts
@@ -3,18 +3,28 @@
 import { customConfig } from '../../custom-config'
 import { LoginCredentials } from './types'
 import { AuthenticationState } from './types/AuthenticationState'
+import { Api, ApiConfig, LoginModel, RequestParams } from './videodb-api'
 
 const apiUrl = customConfig.apiBaseUrl
 
+const vApi = new Api()
+vApi.baseUrl = customConfig.apiBaseUrl? customConfig.apiBaseUrl : 'error'
+
 export async function getToken(credentials : LoginCredentials) {
-  const url = `${apiUrl}/token`
+  const loginModel : LoginModel = {
+    username: credentials.username,
+    password: credentials.password,
+    group: credentials.group
+  }
+  const response = await vApi.token.tokenCreate(loginModel)
+  /*const url = `${apiUrl}/token`
   const response = await fetch (url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(credentials)
-  })
+  })*/
 
   let authState: AuthenticationState = {
     token: '',
@@ -28,6 +38,33 @@ export async function getToken(credentials : LoginCredentials) {
       token: responseJson.token,
       refreshTuple: responseJson.refreshTuple,
       authState: 'AUTHENTICATED'
+    }
+
+    const config = {
+      baseUrl: customConfig.apiBaseUrl? customConfig.apiBaseUrl : 'error',
+      securityWorker: ( token: any ) => {
+        console.log('Security Worker was here.')
+        console.log(`Bearer ${responseJson.token}`)
+        return {
+          headers: {
+            Authorization: `Bearer ${token} `
+          }
+        }
+      }
+    }
+
+    const authApi = new Api(config)
+
+
+
+    try {
+      const info =  await authApi.info.infoIndex({ secure: true })
+      console.log('info')
+      console.log(info)
+    }
+    catch (e) {
+      console.log('y')
+      console.log(e)
     }
   }
 
