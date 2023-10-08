35aa2cadb2ae51bfb31e0a4809e25cfbc54ec859
viper3400 <viper_3400@gmx.de>
2023-10-08 14:47:14 +0200

    session handling with prisma db

---
+++ src/test/prisma.test.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from '@prisma/client'
+
+describe('prisma db client', () => {
+  test('create & delete db entry in HomeWebSession table', async () => {
+    const prisma = new PrismaClient({
+      log: ['query', 'info', 'warn', 'error'],
+    })
+
+    let existingRecords = await prisma.homeWebSession.findMany()
+    expect(existingRecords.length).toBe(0)
+
+    const dbResult = await prisma.homeWebSession.create({
+      data: {
+        username: 'Hans',
+        token: 'xxslj',
+        refresh1: 'xsxs',
+        refresh2: 'AUTH'
+      }
+    })
+    expect(dbResult.username).toBe('Hans')
+
+    existingRecords = await prisma.homeWebSession.findMany()
+    expect(existingRecords.length).toBe(1)
+    expect(existingRecords[0].username).toBe('Hans')
+
+    await prisma.homeWebSession.delete({
+      where: {
+        id: existingRecords[0].id
+      }
+    })
+
+    existingRecords = await prisma.homeWebSession.findMany()
+    expect(existingRecords.length).toBe(0)
+
+  })
+
+})
